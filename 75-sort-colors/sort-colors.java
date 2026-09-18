a20c65a72a601b2248202fa575bd2c4362e69205
guptadolly458 <gupta.dolly353@gmail.com>
2026-09-18 10:53:10 +0530

    Time: 1 ms (18.20%) | Memory: 43.5 MB (58.12%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,6 +1,7 @@
 class Solution {
     public void sortColors(int[] nums) {
        int n = nums.length;
+       /*Bubble Sort
        for(int i = 0;i<n;i++){
         for(int j = 0;j<n-1-i;j++){
             if(nums[j]>nums[j+1]){
@@ -9,6 +10,18 @@ class Solution {
                 nums[j+1] = temp;
             }
         }
-       } 
+       } */
+       //Selection sort
+       for(int i = 0;i<n;i++){
+            int minIndex=i;
+            for(int j = i+1;j<n;j++){
+                if(nums[j]<nums[minIndex]){
+                    minIndex=j;
+                }
+            }
+            int temp = nums[i];
+            nums[i]=nums[minIndex];
+            nums[minIndex]=temp;
+        }
     }
 }
