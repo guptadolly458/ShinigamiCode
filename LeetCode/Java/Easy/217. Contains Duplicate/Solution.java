e879153d90d14e3e018a362a8109c658fff5a498
Dolly Gupta <gupta.dolly353@gmail.com>
2026-09-25 11:00:51 +0530

    Add Easy Java Solution for 217. Contains Duplicate from LeetCode

--- LeetCode/Java/Easy/217. Contains Duplicate/Solution.java
+++ LeetCode/Java/Easy/217. Contains Duplicate/Solution.java
@@ -1,15 +1,7 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
         int n = nums.length;
-        for(int i = 0;i<n-1;i++){
-            for(int j = 0;j<n-i-1;j++){
-                if(nums[j]>nums[j+1]){
-                    int temp = nums[j];
-                    nums[j] = nums[j+1];
-                    nums[j+1] = temp;
-                }
-            }
-        }
+        Arrays.sort(nums);
         for(int i = 0;i<n-1;i++){
            if(nums[i]==nums[i+1]){
             return true;
